c61fac3f529737f5674775a9a6e822d0594767bf
Admin <Admin@DESKTOP-QFVKQ9F>
2022-06-06 15:17:59 +0300

    Add project files.

---
+++ 09. Left and Right Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _09._Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int firstSum = 0;
+            int secondSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                firstSum += number;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                secondSum += number;
+            }
+            if (firstSum==secondSum)
+            {
+                Console.WriteLine($"Yes, sum = {firstSum} ");
+            }
+            else
+            {
+                int difference = firstSum - secondSum;
+                Console.WriteLine($" No, diff = {Math.Abs(difference)}");
+            }
+        }
+    }
+}
